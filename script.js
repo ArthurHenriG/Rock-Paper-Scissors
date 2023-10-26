f04a419e9fa1702c67f99c15b3bf78e859ba8040
ArthurHenriG <81188866+ArthurHenriG@users.noreply.github.com>
2023-10-26 14:44:25 -0300

    player choice function and modified computerChoice player choice is a function that receives the player choice. toLowerCase method is applied to deal with case sensitive problems.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function getComputerChoice(){
-    const OPTIONS=['Rock','Paper','Scissors']
+    const OPTIONS=['Rock','Paper','Scissors'];
     let draw=Math.floor(Math.random() * (3 - 0) + 0);
-    let computerChoice=OPTIONS[draw]
-    console.log(OPTIONS)
+    let computerChoice=OPTIONS[draw];
+    computerChoice=computerChoice.toLowerCase();
     return computerChoice;
+};
+ 
+function playerChoice(){
+    let choice=prompt('Choose rock, paper, scissors');
+    choice=choice.toLowerCase();
+    return choice;
 }
