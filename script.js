59a93107506f0d508cc614211e009d0fb7b0a7a6
ArthurHenriG <81188866+ArthurHenriG@users.noreply.github.com>
2023-10-26 16:34:10 -0300

    Functions playRound and Game playRound check who won the round with basic conditionals. Game execute the game itself, using a loop to execute 5 rounds of the game.

--- script.js
+++ script.js
@@ -10,4 +10,34 @@ function playerChoice(){
     let choice=prompt('Choose rock, paper, scissors');
     choice=choice.toLowerCase();
     return choice;
-}
+};
+
+function playRound(computerSelection, playerSelection){
+    if(computerSelection==='rock' && playerSelection==='scissors'){
+        console.log('You Lose! rock beats scissors');
+    }else if(computerSelection==='paper' && playerSelection==='rock'){
+        console.log('You Lose! paper beats rock');
+    }else if(computerSelection==='scissors' && playerSelection==='paper'){
+        console.log('You Lose! scissors beats paper');
+    }else if(computerSelection==='scissors' && playerSelection==='rock' ){
+        console.log('You Win! rock beats scissors');
+    }else if(computerSelection==='rock' && playerSelection==='paper'){
+        console.log('You Win! paper beats rock');
+    }else if(computerSelection==='paper' && playerSelection==='scissors'){
+        console.log('You Win! scissors beats paper');
+    }else if(computerSelection===playerSelection){
+        console.log('Draw!');
+    }else{
+        console.log("Oops. Check if you didn't type something wrong!");
+    };
+};
+
+function game(){
+    for(let i=0;i<5;i++){
+        let computerSelection=getComputerChoice();
+        let playerSelection=playerChoice();
+        playRound(computerSelection, playerSelection);
+    };
+};
+
+console.log(game());
