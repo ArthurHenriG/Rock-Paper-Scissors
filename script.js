ebc4645709b63d4d3d4103260d7be59e36680448
ArthurHenriG <ArthurHenri00@outlook.com>
2025-01-17 16:05:00 -0300

    Game logic finished added function disableButton that executes when a player reachs 5 points resetGameButton restart the game after a player reachs 5 points Now when the player presses a button the round start The code still need some review and work on UI

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-//var
-const button=document.querySelectorAll("button");
+//Select elements
+const button=document.querySelectorAll(".choiceButton");
 const result=document.querySelector(".result");
-
+const resetGame=document.querySelector(".btn");
 //receives player choice
 let playerSelection;
 //receives computer choice
-let computerSelection=getComputerChoice();
+let computerSelection;
+//count game points 
+let playerScore=0;
+let computerScore=0;
 
 
+//return computer choice
 function getComputerChoice(){
     const OPTIONS=['Rock','Paper','Scissors'];
     let draw=Math.floor(Math.random() * (3 - 0) + 0);
@@ -16,52 +20,73 @@ function getComputerChoice(){
     return computerChoice;
 };
 
-/*
-function playerChoice(value){
-    let choice=value;
-    choice=choice.toLowerCase();
-    return choice;
-};
-*/
 
+//Verify who won the round
+function playRound(computerSelection, playerSelection){
+
+    if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        computerScore++;
+        return 'You Lose! rock beats scissors';
+    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+        computerScore++;
+        return'You Lose! paper beats rock';
+    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        computerScore++;
+        return'You Lose! scissors beats paper';
+    } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+        playerScore++;
+        return 'You Win! rock beats scissors';
+    } else if (computerSelection === 'rock' && playerSelection === 'paper') {
+        playerScore++;
+        return'You Win! paper beats rock';
+    } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+        playerScore++;
+        return 'You Win! scissors beats paper';
+    } else if (computerSelection === playerSelection) {
+         return 'Draw!';
+    } 
+    
+};
+
+//Reset all button's after the game ended 
+resetGame.addEventListener("click",()=>{
+    button.forEach((button)=>{
+        button.disabled=false;
+    }
+    )
+})
+
+//Disable button when a player reach 5 points 
+function disableButton(){
+
+    button.forEach((button)=>{
+        button.disabled=true;
+    }
+    )
+
+}
+
+//Get's user choice and play the round
 button.forEach((button) => {
     button.addEventListener("click",()=>{
-        playerSelection=button.value;
+      playerSelection=button.value;
+      computerSelection=getComputerChoice();
+      result.textContent+= playRound(computerSelection,playerSelection);
+      console.log(playerScore,'pc',computerScore,playerSelection,computerSelection);
+      
+      if(computerScore==5 || playerScore==5){
+         disableButton();
+         computerScore=0;
+         playerScore=0;
+
+         console.log(playerScore,'teste',computerScore)
+      }
     });
+   
 });
 
 
 
-function playRound(computerSelection, playerSelection){
-    if (computerSelection === 'rock' && playerSelection === 'scissors') {
-        return 'You Lose! rock beats scissors';
-    } else if (computerSelection === 'paper' && playerSelection === 'rock') {
-         return'You Lose! paper beats rock';
-    } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-        return'You Lose! scissors beats paper';
-    } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-        return 'You Win! rock beats scissors';
-    } else if (computerSelection === 'rock' && playerSelection === 'paper') {
-         return'You Win! paper beats rock';
-    } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
-        return 'You Win! scissors beats paper';
-    } else if (computerSelection === playerSelection) {
-         return 'Draw!';
-    } else {
-         return "Oops. Check if you didn't type something wrong!";
-    }
-    
-};
 
 
 
-
-const teste=document.querySelector(".teste");
-
-function game(){
-    for(let i=0;i<5;i++){
-        teste.textContent+= playRound(computerSelection, playerSelection);
-    };
-};
-
-game();
